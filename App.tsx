262b880cfa735e76e01e5d48638ea2f59e90f5c1
Maen22 <m3n.ibregeth@gmail.com>
2021-11-06 15:01:50 +0200

    inital redux setup.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-
 import { enableScreens } from "react-native-screens";
-
 import { useFonts } from "expo-font";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
+import mealsReducer from "./src/store/reducers/meals";
 import MealsNavigator from "./src/navigation/MealsNavigator";
 
 enableScreens(false);
+
+// in typical senarios we would have multiple reducers!
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   const [loaded] = useFonts({
@@ -18,5 +26,9 @@ export default function App() {
     return null;
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
