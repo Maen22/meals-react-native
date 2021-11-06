e70259c364688205d0c22267d1c633d23a018159
Maen22 <m3n.ibregeth@gmail.com>
2021-11-06 14:40:03 +0200

    added filters screen content.

--- src/screens/FiltersScreen.tsx
+++ src/screens/FiltersScreen.tsx
@@ -1,14 +1,55 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
 import { DrawerActions } from "react-navigation-drawer";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import Colors from "../constants/Colors";
+
+type FilterSwitchProps = {
+  label: string;
+  state: boolean;
+  onChange: (value: boolean) => void;
+};
+const FilterSwitch: React.FC<FilterSwitchProps> = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        value={props.state}
+        onValueChange={props.onChange}
+        trackColor={{ true: Colors.primary, false: "#aaa" }}
+        thumbColor={Platform.OS === "android" ? Colors.primary : ""}
+      />
+    </View>
+  );
+};
 
 const FiltersScreen: NavigationStackScreenComponent = (props) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegeterian] = useState(false);
+
   return (
-    <View>
-      <Text>Filters Screen</Text>
+    <View style={styles.screen}>
+      <Text style={styles.title}>Available Filters</Text>
+      <FilterSwitch
+        label="Gluten-Free"
+        state={isGlutenFree}
+        onChange={setIsGlutenFree}
+      />
+      <FilterSwitch
+        label="Lactose-Free"
+        state={isLactoseFree}
+        onChange={setIsLactoseFree}
+      />
+      <FilterSwitch label="Vegan" state={isVegan} onChange={setIsVegan} />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={setIsVegeterian}
+      />
     </View>
   );
 };
@@ -26,8 +67,38 @@ FiltersScreen.navigationOptions = (navData) => ({
       ></Item>
     </HeaderButtons>
   ),
+  headerRight: () => (
+    <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item
+        title="Save"
+        iconName="ios-save"
+        onPress={() => {
+          console.log("saved!");
+        }}
+      ></Item>
+    </HeaderButtons>
+  ),
 });
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+
+    alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginVertical: 10,
+  },
+});
 
 export default FiltersScreen;
